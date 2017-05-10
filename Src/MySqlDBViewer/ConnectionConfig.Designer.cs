c4f4ca81f8207279a10287a935c159799f107a3d
何东华 <egman@qq.com>
2017-05-10 22:00:32 +0800

    添加MySql项目

---
+++ Src/MySqlDBViewer/ConnectionConfig.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace DBViewer.Model.MySQL
+{
+    partial class ConnectionConfig
+    {
+        /// <summary> 
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary> 
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.serverName = new System.Windows.Forms.TextBox();
+            this.user = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.password = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dbname = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "服务器IP:";
+            // 
+            // serverName
+            // 
+            this.serverName.Location = new System.Drawing.Point(88, 19);
+            this.serverName.Name = "serverName";
+            this.serverName.Size = new System.Drawing.Size(159, 21);
+            this.serverName.TabIndex = 1;
+            // 
+            // user
+            // 
+            this.user.Location = new System.Drawing.Point(88, 44);
+            this.user.Name = "user";
+            this.user.Size = new System.Drawing.Size(159, 21);
+            this.user.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "用户:";
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(88, 69);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(159, 21);
+            this.password.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "密码:";
+            // 
+            // dbname
+            // 
+            this.dbname.Location = new System.Drawing.Point(88, 94);
+            this.dbname.Name = "dbname";
+            this.dbname.Size = new System.Drawing.Size(159, 21);
+            this.dbname.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 97);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 12);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "数据库:";
+            // 
+            // bindingSource1
+            // 
+            this.bindingSource1.DataSource = typeof(DBViewer.Model.Core.DBViewerConfig);
+            // 
+            // ConnectionConfig
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.dbname);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.user);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.serverName);
+            this.Controls.Add(this.label1);
+            this.Name = "ConnectionConfig";
+            this.Size = new System.Drawing.Size(271, 137);
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox serverName;
+        private System.Windows.Forms.TextBox user;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox dbname;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.BindingSource bindingSource1;
+    }
+}
---
+++ Src/MySqlDBViewer/ConnectionManager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DBViewer.Model.Core;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DBViewer.Model.MySQL
+{
+    class ConnectionManager
+    {
+        public DBViewerConfig config;
+
+        public ConnectionManager(DBViewerConfig config)
+        {
+            this.config = config;
+        }
+
+        public DataTable GetData(string sql)
+        {
+            DataTable table = new DataTable();
+            using (SqlConnection conn = new SqlConnection(GetConnectionString()))
+            {
+                conn.Open();
+                SqlDataAdapter adp = new SqlDataAdapter(sql, conn);
+                adp.Fill(table);
+                conn.Close();
+            }
+            return table;
+        }
+
+
+
+        public void ExecuteCmd(string sql)
+        {
+            using (SqlConnection conn = new SqlConnection(GetConnectionString()))
+            {
+                
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+        }
+        private string GetConnectionString()
+        {
+           return string.Format("Data Source={0};Initial Catalog={1};User Id={2};Password={3}"
+                            ,config.server
+                            ,config.dbname
+                            ,config.user
+                            ,config.password);
+
+        }
+    }
+}
